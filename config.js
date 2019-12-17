fa10445949acc7818c0be38022d6e493bd2dc5aa
Marc Godard <me@marcgodard.com>
2019-12-17 11:57:23 -0500

    Revert "Fixed authentication route"

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 const authUrl = 'https://api.ouraring.com/oauth'
 const baseUrl = 'https://api.ouraring.com/v1'
-const authorizeUrl = 'https://cloud.ouraring.com/oauth'
 module.exports = {
   authUrl: authUrl,
   baseUrl: baseUrl,
   accessTokenUri: authUrl + '/token',
-  authorizationUri: authorizeUrl + '/authorize',
+  authorizationUri: authUrl + '/authorize',
   authorizationGrants: ['credentials'],
   scopes: ['personal', 'daily']
 }
