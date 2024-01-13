e17946e51930ad82f2d1cd4b826caa9409abee7e
Samuel Rinnetmäki <samuel@iki.fi>
2024-01-13 15:19:02 +0200

    added v2 scopes

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   accessTokenUri: 'https://api.ouraring.com/oauth/token',
   authorizationUri: 'https://cloud.ouraring.com/oauth/authorize',
   authorizationGrants: ['credentials'],
-  scopes: ['personal', 'daily', 'heartrate', 'workout', 'tag', 'session']
+  scopes: ['email', 'personal', 'daily', 'heartrate', 'workout', 'tag', 'session', 'spo2']
 }
